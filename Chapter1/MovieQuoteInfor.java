bab70753831a7908bfae04f39a604b6f92731ba5
Lourdyn14 <lourdyndong@gmail.com>
2024-08-14 22:02:44 +0800

    Create MovieQuoteInfor.java

---
+++ Chapter1/MovieQuoteInfor.java
@@ -0,0 +1,10 @@
+public class MovieQuoteInfor 
+  {
+
+public static void main(String[] args) {
+    System.out.println("\n\n\n\t\"In case I don't see ya! good afternoon, good evening, and good night!\"");
+    System.out.println("\tThe Movie: The Truman Show");
+    System.out.println("\tThe Character: Truman Burbank");
+    System.out.println("\tYear:1998");
+    }
+  }
