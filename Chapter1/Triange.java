f078494efeaa521516b5a4e12e85578f8da6d018
Lourdyn14 <lourdyndong@gmail.com>
2024-08-14 22:33:30 +0800

    Create Triange.java

---
+++ Chapter1/Triange.java
@@ -0,0 +1,14 @@
+public class Triangle{
+  public static void main(String[] args){
+      for(int i=1;i<=7;i++){
+          
+        for(int s=i;s<7;s++){
+          System.out.print(" ");
+          }
+        for(int r=1;r<=(2 * i - 1);r++){
+           System.out.print("T");
+          }
+    System.out.println();
+      }
+}  
+}
