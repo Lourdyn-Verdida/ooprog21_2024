3b9143ea0deb7673e63742619b23b715cfe3deda
Verdida,Lourdyn Neil M. <lourdyndong@gmail.com>
2024-09-29 11:01:57 +0800

    Update TestCircle.java

--- Chapter4/TestCircle.java
+++ Chapter4/TestCircle.java
@@ -2,7 +2,6 @@ public class TestCircle {
     public static void main(String[] args) {
         Circle a = new Circle();
         Circle b = new Circle();
-        Circle c = new Circle();
 
         a.setRadius(3);
         System.out.println("First Circle:");
@@ -23,12 +22,6 @@ public class Circle {
     static double radius;
     static double diameter;
     static double area;
-
-      public Circle() {
-        radius = 1;
-        diameter = 2 * radius;
-        area = 3.14 * (radius * radius);
-    }
 
       public static void setRadius(double rad) {
         radius = rad;
