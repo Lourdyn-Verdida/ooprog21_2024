f4815d886dd7ed387bdcb98b9b5c55e27cec8187
Verdida,Lourdyn Neil M. <lourdyndong@gmail.com>
2024-10-03 22:14:35 +0800

    Create MathTest.java

---
+++ Chapter4/MathTest.java
@@ -0,0 +1,21 @@
+public class MathTest {
+    public static void main(String[] args) {
+        System.out.println("SQRT: " + Math.sqrt(37));
+        
+        System.out.println("SIN: " + Math.sin(3.14));
+
+        System.out.println("COS: " + Math.cos(3.14));
+
+        System.out.println("FLOOR: " + Math.floor(22.8));
+
+        System.out.println("CEIL: " + Math.ceil(22.8));
+
+        System.out.println("ROUND: " + Math.round(22.8));
+        
+        System.out.println("MAX: " + Math.max(71, 68));
+
+        System.out.println("MIN: " + Math.min(71, 68));
+        
+        System.out.println("RANDOM: " + Math.random());
+    }
+}
