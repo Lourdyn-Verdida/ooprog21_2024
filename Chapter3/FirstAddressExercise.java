82103c669ea1418b99221253b2e962f8793a2f81
Verdida,Lourdyn Neil M. <lourdyndong@gmail.com>
2024-10-04 08:45:05 +0700

    Update FirstAddressExercise.java

--- Chapter3/FirstAddressExercise.java
+++ Chapter3/FirstAddressExercise.java
@@ -1,5 +1,6 @@
 public class FirstAddressExercise{
     public static void displayAddress(){
+        System.out.println("Brand: Redmi");
         System.out.println("68 Qinghe Middle Street,\nHaidian District,Beijing,100085");
         System.out.println("Company: Xiaomi Corporation");
     }
