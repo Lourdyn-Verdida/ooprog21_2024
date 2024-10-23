55f85efbbd270bcc48e03dc6997e501a75289b43
Verdida,Lourdyn Neil M. <lourdyndong@gmail.com>
2024-10-23 20:47:27 +0800

    Update Ascending.java

--- Chapter5/Ascending.java
+++ Chapter5/Ascending.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
    int num3 = scan.nextInt();
    
    if(num1 < num2 && num2 < num3){
-   System.out.print(""+num3+""+num2+""+num1);
+   System.out.print(""+num1+""+num2+""+num3);
    }else if(num1 < num2 && num3 < num2){
    System.out.print(""+num1+""+num3+""+num2);
    }else if(num2 < num1 && num1 < num3){
@@ -23,6 +23,6 @@ import java.util.Scanner;
    }else if(num3 < num1 && num2 < num1){
    System.out.print(""+num3+""+num1+""+num2);
    }else
-   System.out.print(""+num1+""+num3+""+num2);
+   System.out.print(""+num1+""+num2+""+num3);
    }
 }
